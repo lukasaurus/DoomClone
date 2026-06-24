58d1597a2d0ebacd36ea347de5502b5a15d10cce
lukasaurus <lukasaurus@gmail.com>
2026-06-24 14:29:34 +1200

    added better dungeon gen

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,18 +3,21 @@ extends CharacterBody3D
 @export_category("Movement")
 @export var move_speed : float = 5.0
 @export var gravity : float = 20.0
+@export var jump_force : float = 5.0
 
 @export_category("Look")
 @export var mouse_sensitivity: float = 0.002
 @export var pitch_limit : float = 85.0
 
 @export_category("Shooting")
+var can_shoot = true
+
 @export var fire_rate 	: float = 0.15
 @export var damage 		: float = 25.0
 @export var max_ammo 	: int = 30
 
 @onready var camera  	= $Camera3D
-@onready var raycaast 	= $Camera3D/RayCast3D
+@onready var raycast 	= $Camera3D/RayCast3D
 @onready var gun_sprite = $CanvasLayer/AnimatedSprite2D
 
 var current_ammo 	: int
@@ -47,8 +50,37 @@ func handle_movement():
 		
 func _physics_process(delta: float) -> void:
 	handle_movement()
+	apply_gravity(delta)
+	handle_jump()
+	handle_shooting()#<------------------------
 	move_and_slide()
 	
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+func handle_jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y += jump_force
+			
+func fire():
+	can_fire = false
+	current_ammo -= 1
+	gun_sprite.play("fire")
+	if raycast.is_colliding():
+		var hit_collider = raycast.get_collider()
+		print(hit_collider.name) #<------------------------- ADD THIS
+		if hit_collider.has_method("take_damage"):
+			hit_collider.take_damage(damage)
+	await gun_sprite.animation_finished
+	can_fire = true
+	gun_sprite.play("default")
+
+func handle_shooting():
+	var should_fire = Input.is_action_just_pressed("shoot")
+	if should_fire and can_fire and current_ammo > 0:
+		fire()
 	
 	
 	
